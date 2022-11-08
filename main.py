6900b016d76e3ae688db167d8448e0fca174c515
Rohit Kushwah <rohitkushwah1742000@gmail.com>
2022-11-09 00:50:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,4 +9,4 @@ def prediction():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=4000)
