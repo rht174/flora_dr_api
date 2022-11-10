14c5caeb79f2847299bf85642213a32dd37611c7
Rohit Kushwah <rohitkushwah1742000@gmail.com>
2022-11-11 02:56:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/predict', methods=['GET'])
+@app.route('/predict', methods=['GET', 'POST'])
 def prediction():
     if request.method == 'POST':
         imagefile = request.files['image']
@@ -12,4 +12,4 @@ def prediction():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4000)
+    app.run(debug=True, port=8000)
