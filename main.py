063c12d008191bed610240716e1d4e8abdf80d21
Rohit Kushwah <rohitkushwah1742000@gmail.com>
2022-11-13 22:45:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,13 @@ from keras.applications.mobilenet_v2 import decode_predictions
 
 app = Flask(__name__)
 
-plant_dict = {'apple': 'Apple', 'cherry': 'Cherry', 'corn': 'Corn', 'grape': 'Grape', 'pepper_bell': 'Paper Bell',
-              'potato': 'Potato',
-              'strawberry': 'Strawberry', 'tea': 'Tea', 'tomato': 'Tomato'}
+plant_dict = {'apple': 'Apple', 'corn': 'Corn', 'grape': 'Grape', 'tea': 'Tea', 'tomato': 'Tomato'}
 
 classes_dict = {'apple': ['Scab', 'Black Rot', 'Cedar Apple Rust', 'Healthy'],
-                'cherry': ['Healthy', 'Powdery Mildew'],
                 'corn': ['Blight', 'Common Rust', 'Gray Leaf Spot', 'Healthy'],
                 'grape': ['Black Rot', 'Black Measles', 'Healthy', 'Leaf Blight Isariopsis Leaf Spot'],
-                'pepper_bell': ['Bacterial Spot', 'Healthy'],
-                'potato': ['Early Blight', 'Healthy', 'Late Blight'],
-                'strawberry': ['Healthy', 'Leaf Scorch'],
-                'tea': ['Agal Leaf', 'Anthracnose', 'Bird Eye Spot', 'Brown Blight', 'Healthy', 'Red Leaf Spot'],
-                'tomato': ['Late Blight', 'Bacterial Spot', 'Early Blight', 'Healthy', 'Leaf Mold', 'Mosaic Virus',
-                           'Septoria Leaf Spot', 'Spider Mites Two Spotted Spider Mite', 'Target Spot']}
+                'tea': ['Bird Eye Spot', 'Brown Blight', 'Healthy', 'Red Leaf Spot'],
+                'tomato': ['Late Blight', 'Bacterial Spot', 'Early Blight', 'Healthy']}
 
 
 @app.route('/', methods=['GET'])
