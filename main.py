a2f4d9bf5b1760cc156595a9962013181766a42c
Rohit Kushwah <rohitkushwah1742000@gmail.com>
2022-11-13 19:51:51 +0530

    update

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ from keras.applications.mobilenet_v2 import decode_predictions
 
 app = Flask(__name__)
 
+plant_dict = {'apple': 'Apple', 'cherry': 'Cherry', 'corn': 'Corn', 'grape': 'Grape', 'pepper_bell': 'Paper Bell',
+              'potato': 'Potato',
+              'strawberry': 'Strawberry', 'tea': 'Tea', 'tomato': 'Tomato'}
+
+classes_dict = {'apple': ['Scab', 'Black Rot', 'Cedar Apple Rust', 'Healthy'],
+                'cherry': ['Healthy', 'Powdery Mildew'],
+                'corn': ['Blight', 'Common Rust', 'Gray Leaf Spot', 'Healthy'],
+                'grape': ['Black Rot', 'Black Measles', 'Healthy', 'Leaf Blight Isariopsis Leaf Spot'],
+                'pepper_bell': ['Bacterial Spot', 'Healthy'],
+                'potato': ['Early Blight', 'Healthy', 'Late Blight'],
+                'strawberry': ['Healthy', 'Leaf Scorch'],
+                'tea': ['Agal Leaf', 'Anthracnose', 'Bird Eye Spot', 'Brown Blight', 'Healthy', 'Red Leaf Spot'],
+                'tomato': ['Late Blight', 'Bacterial Spot', 'Early Blight', 'Healthy', 'Leaf Mold', 'Mosaic Virus',
+                           'Septoria Leaf Spot', 'Spider Mites Two Spotted Spider Mite', 'Target Spot']}
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -19,11 +34,21 @@ def prediction():
         imagefile = request.files['image']
         temp_path = 'temp/temp_image.jpg'
         imagefile.save(temp_path)
-        # return jsonify({'message': 'image received successfully'})
+        plant = request.form['plant']
 
-        model = keras.models.load_model('models/apple.h5')
+        model = keras.models.load_model(f'models/{plant}.h5')
+        img = tf.keras.preprocessing.image.load_img(temp_path, target_size=(224, 224))
+        img = tf.keras.utils.img_to_array(img)
+        img = img / 255
+        img = np.array([img])
+        pred = model.predict(img)
+        condition = np.argmax(pred)
+        confidence = round(100 * (np.max(pred)), 2)
 
-        return render_template('index.html')
+        health = classes_dict[plant][condition]
+
+        return render_template('index.html', plant_name=plant_dict[plant], plant_condition=health,
+                               pred_confidence=confidence)
 
 
 if __name__ == "__main__":
